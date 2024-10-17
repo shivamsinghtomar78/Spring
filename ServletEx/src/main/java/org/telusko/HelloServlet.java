3bdb300f997e6e351b8993208d32d759ec168d7e
shivamsinghtomar78 <139133103+shivamsinghtomar78@users.noreply.github.com>
2024-10-17 22:24:14 +0530

    Add files via upload

---
+++ ServletEx/src/main/java/org/telusko/HelloServlet.java
@@ -0,0 +1,18 @@
+package org.telusko;
+
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class HelloServlet extends HttpServlet {
+    public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+        System.out.println("In service");
+        res.setContentType("text/html");
+        PrintWriter out=res.getWriter();
+        out.println("<h2><b> Hello wala world</b></h2>");
+    }
+
+}
