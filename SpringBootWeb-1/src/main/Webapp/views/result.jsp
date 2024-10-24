1b47cab4e9c52cef2a5ef3651b52028e396bc05f
shivamsinghtomar78 <139133103+shivamsinghtomar78@users.noreply.github.com>
2024-10-24 17:01:59 +0530

    Add files via upload

---
+++ SpringBootWeb-1/src/main/Webapp/views/result.jsp
@@ -0,0 +1,15 @@
+<%@page language="java" %>
+
+<html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="style.css">
+    </head>
+    </body>
+
+
+        <h2>Welcome To Company </h2>
+        <p> ${alien} </p>
+        <p> Welcome to  ${alien} </p>
+
+    </body>
+</html>
---
+++ SpringBootWeb-1/src/main/java/com/telusko/SpringBootWeb1/HomeController.java
@@ -0,0 +1,32 @@
+package com.telusko.SpringBootWeb1;
+
+import org.springframework.stereotype.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public String home() {
+        return "index";
+    }
+
+    @RequestMapping("add")
+    public ModelAndView add(@RequestParam("num1") int num1, @RequestParam("num2") int num2, ModelAndView mv) {
+
+        int result = num1 + num2;
+        mv.addObject("result",result);
+        mv.setViewName("result");
+
+        return mv;
+    }
+    @RequestMapping("addAlien")
+    public  String addAlien( Alien alien){
+        return "result";
+    }
+
+
+}
